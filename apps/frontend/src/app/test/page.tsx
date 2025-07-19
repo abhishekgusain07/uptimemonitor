0f9efc93067562a4cbabc12a3c83e23ab0c4a1cc
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-07-19 16:13:40 +0530

    testing something"

---
+++ apps/frontend/src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import Checking from "@repo/ui/checking"
+
+const TestPage = () => {
+    return (
+        <div className="flex justify-center items-center">
+            <Checking />
+        </div>
+    )
+}
+export default TestPage;
---
+++ packages/ui/src/checking.tsx
@@ -0,0 +1,8 @@
+const Checking = () => {
+    return (
+        <div className="text-lg">
+            This is imported from packages
+        </div>
+    )
+}
+export default Checking;
